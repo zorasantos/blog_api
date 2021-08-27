b9e13fab61bf1edf41f06debbc5e6db2185853fb
Zoranildo Santos <zoranildo.santos@pge.ce.gov.br>
2021-08-27 09:07:28 -0300

    Versionamento

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
-  resources :articles
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    scope module: :v1,
+      constraints: ApiConstraints.new(version: 1, default: true) do
+      resources :articles
+    end
+  end
 end
